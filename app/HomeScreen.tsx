fdd6f9e8071433cc1a84d5b2bf6961a95cd60736
unknown <pf1963@fiap.com.br>
2026-08-20 10:46:05 -0300

    Implementação o botão de realizar logoff

--- app/HomeScreen.tsx
+++ app/HomeScreen.tsx
@@ -1,10 +1,25 @@
-import { Text } from "react-native";
+import { Text, Button } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { useRouter } from "expo-router";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+
 
 export default function HomeScreen() {
+    const router = useRouter();
+
+    const realizarLogoff = async () => {
+        await AsyncStorage.removeItem("@user")
+        router.replace("/")
+    }
+
     return (
         <SafeAreaView>
             <Text>Olá, Você está na Tela Inicial</Text>
+            <Button
+                title="Sair da Conta"
+                onPress={realizarLogoff}
+            />
         </SafeAreaView>
     )
 }
